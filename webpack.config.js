5072e9f4d806fac3172484b6942c6feb0fadac9c
Pascal Pencil <pascal@superpencil.com>
2016-11-20 22:52:32 +0900

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devtool: 'source-map',
+const path = require('path');
+const merge = require('webpack-merge')
+
+const PATHS = {
+  build: path.join(__dirname, 'build'),
+  dev: __dirname
+}
+
+const common = {
+  entry: ['./src/index.js'],
   module: {
     loaders: [
       {
@@ -17,12 +17,10 @@ module.exports = {
         query: {
           presets: ['react', 'es2015', 'stage-1']
         }
-      },
-      {
+      }, {
         test: /\.(sass|scss)$/,
         loaders: ['style', 'css', 'sass']
-      },
-      {
+      }, {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
         loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
       }
@@ -31,8 +29,33 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx', '.sass']
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+  devtool: 'source-map'
 }
+
+var config
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+  case 'stats':
+    config = merge(common, {
+      output: {
+        path: PATHS.build,
+        publicPath: '/five-forecast/',
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[chunkhash].js'
+      }
+    })
+  default:
+    config = merge(common, {
+      output: {
+        path: PATHS.dev,
+        publicPath: '/',
+        filename: 'bundle.js'
+      },
+      devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+      }
+    })
+}
+
+module.exports = config
